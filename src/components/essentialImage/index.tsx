3a7341cdb09916fda22963ed6e86d75a0f11785c
yeguu <qsc130@gmail.com>
2023-12-02 20:03:21 +0900

    fix: 이미지 컴포넌트 생성

---
+++ src/components/essentialImage/index.tsx
@@ -0,0 +1,9 @@
+import { EssentialImageStyle } from "./styles";
+
+import TestImage from "../../assets/FrontEnd-Test-Image.png";
+
+const EssentialImage = () => {
+  return <EssentialImageStyle src={TestImage} />;
+};
+
+export default EssentialImage;
