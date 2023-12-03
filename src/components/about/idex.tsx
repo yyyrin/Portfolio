f4c50eef373bacd7d41b6d46497504ae3f7b1f38
yeguu <qsc130@gmail.com>
2023-12-04 02:40:10 +0900

    feat: About 페이지 구현

--- src/components/about/idex.tsx
+++ src/components/about/idex.tsx
@@ -1,7 +1,80 @@
-import { AboutContainer } from "./styles";
+import {
+  AboutContainer,
+  BoxStyle,
+  ContentContainer,
+  IconAndTextContainer,
+  IconStyle,
+  LineStyle,
+  ProfileImageStyle,
+  TextStyle,
+  TitleStyle,
+  DivideLineStyle,
+} from "./styles";
+import ProfileImage from "../../assets/profileImg.png";
+import PersonIc from "../../assets/personIc.svg";
+import CalendarIc from "../../assets/calendarIc.svg";
+import PlaceIc from "../../assets/placeIc.svg";
+import EmailIc from "../../assets/emailIc.svg";
+import SchoolIc from "../../assets/schoolIc.svg";
+import GithubIc from "../../assets/githubIc.svg";
+import DocumentIc from "../../assets/documentIc.svg";
 
 const About = () => {
-  return <AboutContainer id="about"></AboutContainer>;
+  const handleLinkClick = (linkType: string) => {
+    if (linkType === "github") {
+      window.location.href = "https://github.com/yyyrin";
+    }
+    if (linkType === "blog") {
+      window.location.href = "https://velog.io/@yeguu037";
+    }
+  };
+
+  return (
+    <AboutContainer id="about">
+      <TitleStyle>About</TitleStyle>
+      <LineStyle />
+      <BoxStyle>
+        <ProfileImageStyle src={ProfileImage} />
+        <ContentContainer>
+          <IconAndTextContainer>
+            <IconStyle src={PersonIc} />
+            <TextStyle>최예린</TextStyle>
+          </IconAndTextContainer>
+          <IconAndTextContainer>
+            <IconStyle src={CalendarIc} />
+            <TextStyle>1998.08.29</TextStyle>
+          </IconAndTextContainer>
+          <IconAndTextContainer>
+            <IconStyle src={PlaceIc} />
+            <TextStyle>경상북도 경산시</TextStyle>
+          </IconAndTextContainer>
+          <IconAndTextContainer>
+            <IconStyle src={EmailIc} />
+            <TextStyle>qsc130@naver.com</TextStyle>
+          </IconAndTextContainer>
+          <IconAndTextContainer>
+            <IconStyle src={SchoolIc} />
+            <TextStyle>경기대학교 (응용통계학과, 경영학과)</TextStyle>
+          </IconAndTextContainer>
+          <DivideLineStyle />
+          <IconAndTextContainer
+            className="github"
+            onClick={() => handleLinkClick("github")}
+          >
+            <IconStyle src={GithubIc} />
+            <TextStyle>GitHub</TextStyle>
+          </IconAndTextContainer>
+          <IconAndTextContainer
+            className="blog"
+            onClick={() => handleLinkClick("blog")}
+          >
+            <IconStyle src={DocumentIc} />
+            <TextStyle>Blog</TextStyle>
+          </IconAndTextContainer>
+        </ContentContainer>
+      </BoxStyle>
+    </AboutContainer>
+  );
 };
 
 export default About;
--- src/components/about/styles.ts
+++ src/components/about/styles.ts
@@ -1,10 +1,79 @@
 import { styled } from "styled-components";
+import { COLOR } from "../../constants/colors";
 
 export const AboutContainer = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: red;
+  background: linear-gradient(180deg, #001b79 0%, #1640d6 100%);
+`;
+
+export const TitleStyle = styled.div`
+  color: white;
+  text-align: center;
+  font-size: 55px;
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
+`;
+
+export const LineStyle = styled.div`
+  width: 200px;
+  border: 2px solid white;
+  margin: 2px auto 30px;
+`;
+
+export const BoxStyle = styled.div`
+  background-color: white;
+  justify-content: center;
+  align-items: center;
+  gap: 60px;
+  display: inline-flex;
+  flex-direction: row;
+  border-radius: 50px;
+  padding: 60px 80px;
+  box-shadow: 0px 10px 10px 4px rgba(0, 0, 0, 0.4);
+`;
+
+export const ProfileImageStyle = styled.img`
+  width: 240px;
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 14px;
+`;
+
+export const IconAndTextContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+
+  &.github,
+  &.blog {
+    cursor: pointer;
+  }
+`;
+
+export const IconStyle = styled.img`
+  height: 30px;
+  width: 30px;
+`;
+
+export const TextStyle = styled.div`
+  color: black;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const DivideLineStyle = styled.div`
+  width: 350px;
+  border: 1px solid ${COLOR.Main_Blue};
 `;
