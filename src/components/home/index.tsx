1d4cb96cbc39749c44ec0a43358e4ace37623c42
yeguu <qsc130@gmail.com>
2023-12-03 20:27:56 +0900

    feat: 포트폴리오 첫 번째 페이지 구현 및 화살표 아이콘 클릭 시 다음 페이지로 이동하도록 설정

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,9 +1,36 @@
 import mainImage from "../../assets/mainImg.png";
-import { HomeContainer, MainImageStyle } from "./styles";
+import {
+  HomeContainer,
+  ContentContainer,
+  MainFontStyle,
+  LineStyle,
+  SideFontStyle,
+  MainImageStyle,
+  IconContainer,
+  ArrowIcStyle,
+} from "./styles";
+import ArrowIc from "../../assets/arrowIc.svg";
 
 const Home = () => {
   return (
     <HomeContainer>
+      <ContentContainer>
+        <MainFontStyle>
+          - 최예린 -<br />
+          매일 1px씩 성장하는 개발자
+        </MainFontStyle>
+        <LineStyle />
+        <SideFontStyle>
+          안녕하세요 :)
+          <br />
+          프론트엔드 개발자 최예린입니다.
+          <br />
+          공부한 내용을 매일 기록하고, 동료들과 지식을 나누는 것을 좋아합니다.
+        </SideFontStyle>
+      </ContentContainer>
+      <IconContainer to="/#about">
+        <ArrowIcStyle src={ArrowIc} />
+      </IconContainer>
       <MainImageStyle src={mainImage} />
     </HomeContainer>
   );
--- src/components/home/styles.ts
+++ src/components/home/styles.ts
@@ -1,16 +1,64 @@
 import { styled } from "styled-components";
+import { COLOR } from "../../constants/colors";
+import { NavLink } from "react-router-dom";
 
 export const HomeContainer = styled.div`
   width: 100%;
   height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  position: relative;
   background-color: black;
+`;
+
+export const ContentContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -40%);
+  z-index: 1;
+  text-align: center;
+`;
+
+export const MainFontStyle = styled.div`
+  color: white;
+  font-size: 45px;
+  font-weight: 700;
+  text-align: center;
+  font-style: normal;
+  line-height: 1.6;
+`;
+
+export const LineStyle = styled.div`
+  width: 108px;
+  border: 1.5px solid ${COLOR.Side_Blue};
+  margin: 40px auto 20px;
+`;
+
+export const SideFontStyle = styled.div`
+  color: white;
+  font-size: 18px;
+  font-weight: 300;
+  text-align: center;
+  font-style: normal;
+  line-height: 1.5;
+`;
+
+export const IconContainer = styled(NavLink)`
+  position: absolute;
+  top: 90%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+`;
+
+export const ArrowIcStyle = styled.img`
+  width: 70px;
+  height: 70px;
+  cursor: pointer;
 `;
 
 export const MainImageStyle = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
+  z-index: 0;
 `;
