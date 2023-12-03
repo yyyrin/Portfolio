790f093887964665a10f6058568888a326088d3a
yeguu <qsc130@gmail.com>
2023-12-03 17:56:36 +0900

    fix: 포트폴리오 페이지 router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ImagePage } from "./pages";
+import { ImagePage, PortfolioPage } from "./pages";
+import Header from "./components/common/header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
+        <Route path="/" element={<PortfolioPage />}></Route>
         <Route path="/image" element={<ImagePage />}></Route>
       </Routes>
     </BrowserRouter>
