addb9ced5a828ce141fac7a166079636f9c9f158
yeguu <qsc130@gmail.com>
2023-12-04 20:28:48 +0900

    fix: main.tsx에 render 문제 해결

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element with id 'root' not found.");
+}
