e3f619fa4ccc93aced61a6cd6637e2bff2a6916f
yeguu <qsc130@gmail.com>
2023-12-04 18:47:26 +0900

    feat: projects 컴포넌트 구현

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,7 +1,23 @@
-import { ProjectsContainer } from "./styles";
+import Dawool from "./Dawool";
+import Poppop from "./Poppop";
+import {
+  BoxGroupContainer,
+  LineStyle,
+  ProjectsContainer,
+  TitleStyle,
+} from "./styles";
 
 const Projects = () => {
-  return <ProjectsContainer id="projects"></ProjectsContainer>;
+  return (
+    <ProjectsContainer id="projects">
+      <TitleStyle>Projects</TitleStyle>
+      <LineStyle />
+      <BoxGroupContainer>
+        <Poppop />
+        <Dawool />
+      </BoxGroupContainer>
+    </ProjectsContainer>
+  );
 };
 
 export default Projects;
